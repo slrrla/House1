ef50128a1828b9506acb69491d8cad6b5831110f
junybaik <junybaik@gmail.com>
2021-04-21 10:55:30 -0400

    update1

--- ver1/App.js
+++ ver1/App.js
@@ -1,43 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style = {styles.Flex}>
-        <Text style= {styles.Text}>방한칸 외계인</Text>
-      </View>
-
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      
-
-      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  Flex:{
-    flex :1,
-    backgroundColor :"7A4988"
-  
-    
-
-  },
-  Text:{
-    textAlign:"left",
-    Colors: "#fff"
-  }
-  
-
-  
 });
